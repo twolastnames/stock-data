12930b718692c7ae5e7233fde598a50be5580f47
Alan Glenn <aglenn@groupon.com>
2020-04-15 14:32:00 -0500

    feat: proxy data feed

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/v1/stock', createProxyMiddleware({ target: 'https://cloud.iexapis.com', changeOrigin: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
