a2e0644db52030251ad7de90fc9a54835e5c8b7d
Alan Glenn <aglenn@groupon.com>
2020-04-15 20:09:35 -0500

    feat: make symbol calls

--- src/SymbolInput.js
+++ src/SymbolInput.js
@@ -1,17 +1,76 @@
 import React from 'react';
+import ReactDom from 'react-dom'
 import './App.css';
 
+function getDisplayString({name, symbol}) {
+  return `${name} (${symbol})`
+}
+
+function DataSet(props) {
+  const options = props.suggestions.map(text => {
+    return <option value={text} />
+  })
+  return (
+    <datalist id="suggestions">
+      {options}
+    </datalist>
+  )
+}
+
 function SymbolInput(props) {
-    const produceSymbol = () => {
-      props.symbolListener('mine', 'm2')
+  let currentSuggestions = []
+  let lastValue = ''
+  let inFlight = false
+  const produceSymbol = async () => {
+    if(inFlight) {
+      return
     }
-    return (
-      <form>
-        <label>Stock Symbol</label>
-        <input type="text" name="symbol"/>
-        <input type="button" value="Submit" onClick={produceSymbol}/>
-      </form>
-    )
+    const value = document.querySelector('[name="symbol"]').value.trim()
+    if(value === lastValue) {
+      return
+    }
+    lastValue = value
+    if(value.length < 3) {
+      currentSuggestions = []
+      ReactDom.render(<DataSet suggestions={currentSuggestions} />, document.getElementById('suggestion-container'))
+      return
+    }
+    const match = value.match(/^(.+) \((.+)\)$/)
+    if(!match || !currentSuggestions.includes(value)) {
+      try {
+        inFlight = true
+        const response = await fetch(`/symbol/${value}`)
+        const similarMatches = await response.json()
+        currentSuggestions = similarMatches.map(getDisplayString)
+        await ReactDom.render(
+          <DataSet suggestions={currentSuggestions} />,
+          document.getElementById('suggestion-container'),
+          () => inFlight = false
+        )
+        return
+      } catch {
+        inFlight = false
+      }
+    }
+    const symbol = match[2]
+    const name = match[1]
+    
+    document.querySelector('[name="symbol"]').value = ''
+    lastValue = ''
+    props.symbolListener(match[2], match[1])
+  }
+
+  setInterval(produceSymbol, 500)
+
+  return (
+    <div>
+      <div id="suggestion-container">
+      <datalist id="suggestions" />
+      </div>
+      <label>Company Name</label>
+      <input type="text" list="suggestions" name="symbol" autoComplete="off"/>
+    </div>
+  )
 }
 
 export default SymbolInput
